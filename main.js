abb2376a73241ae09cbfe8fdbf47469c613e612a
Rahat Polash <100351668+rahatr1@users.noreply.github.com>
2022-08-10 13:51:45 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 
+var buttonNumber = document.querySelectorAll(".drum").length;
 
-document.querySelectorAll("button")[].addEventListener("click", handleClick);
 
-function handleClick() {
-    alert("I got clicked!");
+for (var i = 0; i<buttonNumber; i++)  {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+    function handleClick() {
+        alert("I got clicked!");
+    }
 }
+
